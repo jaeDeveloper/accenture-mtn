439353d5e7c3e240ec2a0a98c4010a3e288cb193
jaeDeveloper <bandile@bandileweb.co.za>
2021-03-19 13:54:24 +0200

    updated header styles & added store page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StoreComponent } from './components/store/store.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: StoreComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
